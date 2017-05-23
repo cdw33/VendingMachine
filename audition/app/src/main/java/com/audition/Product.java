58dd0191775cc4f6583edbe2c1495f449a28fb18
Christopher Wilson <christopher.d.wilson@gmail.com>
2017-05-23 16:36:07 -0400

    Created Product object class

---
+++ audition/app/src/main/java/com/audition/Product.java
@@ -0,0 +1,13 @@
+package com.audition;
+
+public class Product {
+    String name;
+
+    Product(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
